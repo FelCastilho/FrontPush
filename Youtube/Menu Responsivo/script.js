d1ef4be0591f2b837b71b521ce0bf45882663255
felipeCast <castfelipe10@gmail.com>
2024-11-11 07:51:59 -0300

    first commit

---
+++ Youtube/Menu Responsivo/script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.hamburguer').addEventListener('click', function() {
+    this.classList.toggle('active')
+    document.querySelector('.menu').classList.toggle('active')
+})
