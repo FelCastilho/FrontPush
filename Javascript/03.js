aae42f63dc9366db35adfe9011769bc440bebf46
felipeCast <castfelipe10@gmail.com>
2025-01-31 08:02:43 -0300

    iniciando estudos em javascript

---
+++ Javascript/03.js
@@ -0,0 +1,9 @@
+//Variaveis e operadores
+
+// Var, let e const
+
+//O var nos permite usar as variaveis em qualquer lugar do código
+
+//Let é restrito por blocos e escopos, ao declarar dentro de uma função, ela só funcionará lá
+
+//Const cria uma variavel única, o valor dela será constante e não poderá ser trocado
